2f545d63eaaf4bf9091fa416b578d0c13112b095
Charlie Root <root@files2.yyc.globalbotanical.com>
2012-09-19 23:46:11 -0400

    repaired minor syntax differences between linux and bsd

--- sampleSchemaConversionScript.sh
+++ sampleSchemaConversionScript.sh
@@ -10,28 +10,28 @@ cat $inFile | ./ldifJoin > $tmpFile
 ##########################################################
 # avmaxServer -> gtServer
 ##########################################################
-sed -i 's/avmaxServer/gtServer/g'         $tmpFile
+sed -i -e 's/avmaxServer/gtServer/g'         $tmpFile
 
-sed -i 's/^asFQDN/gtsFQDN/g'              $tmpFile
-sed -i 's/^asName/gtsName/g'              $tmpFile
-sed -i 's/^asNickname/gtsNickname/g'      $tmpFile
+sed -i -e 's/^asFQDN/gtsFQDN/g'              $tmpFile
+sed -i -e 's/^asName/gtsName/g'              $tmpFile
+sed -i -e 's/^asNickname/gtsNickname/g'      $tmpFile
 
-sed -i 's/asName=/gtsName=/g'             $tmpFile
+sed -i -e 's/asName=/gtsName=/g'             $tmpFile
 
 
 ##########################################################
 # avmaxServer -> gtServer
 ##########################################################
-sed -i 's/avmaxWorkspace/gtWorkspace/g'         $tmpFile
+sed -i -e 's/avmaxWorkspace/gtWorkspace/g'         $tmpFile
 
-sed -i 's/^awsServer/gtwsServer/g'              $tmpFile
-sed -i 's/^awsName/gtwsName/g'                  $tmpFile
-sed -i 's/^awsRelativePath/gtwsRelativePath/g'  $tmpFile
-sed -i 's/^awsACL/gtwsACL/g'                    $tmpFile
-sed -i 's/^awsLinkFile/gtwsLinkFile/g'          $tmpFile
-sed -i 's/^awsOwnerUid/gtwsOwnerUid/g'          $tmpFile
+sed -i -e 's/^awsServer/gtwsServer/g'              $tmpFile
+sed -i -e 's/^awsName/gtwsName/g'                  $tmpFile
+sed -i -e 's/^awsRelativePath/gtwsRelativePath/g'  $tmpFile
+sed -i -e 's/^awsACL/gtwsACL/g'                    $tmpFile
+sed -i -e 's/^awsLinkFile/gtwsLinkFile/g'          $tmpFile
+sed -i -e 's/^awsOwnerUid/gtwsOwnerUid/g'          $tmpFile
 
-sed -i 's/awsName=/gtwsName=/g'                 $tmpFile
+sed -i -e 's/awsName=/gtwsName=/g'                 $tmpFile
 
 ##########################################################
 # finish up
